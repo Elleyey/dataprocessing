76da160743887055d56e76ecea369733d1ba75f7
Elleyey <ellemijke@hotmail.com>
2018-04-25 12:11:06 +0200

    alles in deze nieuwe repo

---
+++ Homework/week_3/convertCSV2JSON.py
@@ -0,0 +1,13 @@
+import csv
+import json
+import sys
+
+# first try to get the data i need rom obesity.csv from the WHO
+# which is obesity % from both sexes
+
+with open('obesitylim.csv', 'r') as csvObesity:
+    csvObesityReader = csv.reader(csvObesity)
+
+    with open('obesitas.json', 'w') as jsonObesity:
+        for line in csvObesityReader:
+            json.dump((line[0], line[1][:4], line[2][:4]), jsonObesity)
