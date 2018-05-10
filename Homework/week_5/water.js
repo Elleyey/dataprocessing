d43459dbaec04cebe7fb60e3684fb368c445790e
Elleyey <ellemijke@hotmail.com>
2018-05-10 19:39:45 +0200

    data van noord amerika

--- Homework/week_5/water.js
+++ Homework/week_5/water.js
@@ -21,6 +21,7 @@ window.onload = function()
       for (var i = 0; i < countriesLength; i++)
        {
          var countryName = obesitySimple["fact"][i]["dims"]["COUNTRY"];
+         console.log(countryName);
          var obesityValue = obesitySimple["fact"][i]["Value"];
          var waterValue = waterSimple["fact"][i]["Value"];
 
@@ -32,19 +33,43 @@ window.onload = function()
              });
        }
 
-       // open map of South America
-    //    var map = d3.xml("southAmerica.svg").mimeType("image/svg+xml").get(function(error, xml) {
-    //      if (error) throw error;
-    //      document.body.appendChild(xml.documentElement);
-    //    });
-    //
-    // var peru = document.getElementById("Peru")
-    //                     .fill("red");
+    var map = new Datamap({
+      element: document.getElementById("container"),
+      scope: 'world',
+      // zoom in on North America
+      setProjection: function(element) {
+        var projection = d3.geo.equirectangular()
+                            .center([-50, -25])
+                            .rotate([4.4, 0])
+                            .scale(200)
+                            .translate([element.offsetWidth / 2, element.offsetHeight / 2]);
+        var path = d3.geo.path()
+                      .projection(projection);
 
-    var map = new Datamap({element: document.getElementById('container')});
+        return {path: path, projection: projection};
+      },
+      fills: {
+        defaultFill: 'rgb(255, 255, 255)',
+        NorthAmerica: 'rgba(255, 105, 180, 0.8)'
+      },
+      data: {
+        PER: { fillKey: 'NorthAmerica'},
+        ARG: { fillKey: 'NorthAmerica'},
+        BOL: { fillKey: 'NorthAmerica'},
+        BRA: { fillKey: 'NorthAmerica'},
+        CHL: { fillKey: 'NorthAmerica'},
+        COL: { fillKey: 'NorthAmerica'},
+        ECU: { fillKey: 'NorthAmerica'},
+        GUY: { fillKey: 'NorthAmerica'},
+        VEN: { fillKey: 'NorthAmerica'},
+        URY: { fillKey: 'NorthAmerica'},
+        SUR: { fillKey: 'NorthAmerica'},
+        GUF: { fillKey: 'defaultFill'},
+        PRY: { fillKey: 'NorthAmerica'},
+        PAN: { fillKey: 'NorthAmerica'},
 
-
-
+      }
+    });
 
 // close doFunction
 };
